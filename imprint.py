9ebe368f3dec105c9cfba6dc68bacc83c27ee56b
Abdullah P Rashed Ahmed <apra@slac.stanford.edu>
2016-07-11 13:35:52 -0700

    More or less finished the overall script. Still has errors though.

--- imprint.py
+++ imprint.py
@@ -4,16 +4,17 @@ import os
 import numpy as np
 from psp import Pv
 from itertools import chain, izip
-from blbase.iterscan import IterScan, _test_motor_factory
+from blbase.iterscan import IterScan
 from blbase.motor import Motor
 # from blbase.virtualmotor import VirtualMotor     #Commented out until fixed
 from blinst.linac import Linac
 from ConfigParser import SafeConfigParser
 from docopt import docopt
 from ast import literal_eval
+from math import isnan
 
 defaultPath = "imprintConfigurations/imprintStandard.cfg"
-PV_Attenuator = "GATT:FEE1:310:P_DES"
+pvAttenuator = "GATT:FEE1:310:P_DES"
 
 class Imprint(object):
 	"""
@@ -21,7 +22,6 @@ class Imprint(object):
 	"""
 	def __init__(self, **kwargs):
 		"""Checks the file path and initializes the imprint parameters"""
-		
 		self._path = kwargs.get("path", defaultPath)
 		self._burstMode = kwargs.get("burstMode", False)
 		self._useMotors = kwargs.get("test", True)
@@ -29,41 +29,19 @@ class Imprint(object):
 		self._verbose = kwargs.get("verbose", False)
 		
 		self._initParams()
-		self._linac = self._initLinac()
 	
 	def _initParams(self):
 		"""
 		Parses the config file and initializes the motors, iterators, hooks, and
 		IterScan object
 		"""
+		# Maybe move this back into the init
 		self._parseConfig()
 		self._checkConfig()
 		self._positionIterators = self._initIterators()
-
-		self._a_iterator = chain.from_iterable([float(x)]*self.xRange for x in self.attenuatorString.split())
-		self._iteratorY = (self.yfrom + i*self.deltaY for i in range(self.yRange))
-		self._iteratorZ = (self.zfrom + i*self.deltaZ for i in range(self.yRange))
-		self._iterators = [(self.xfrom + i*self.deltaX for i in range(self.xRange)), 
-						   izip(self._iteratorY, self._iteratorZ)]
-		self._xPositions = list(self._iterators[0])
-		self._yPositions = list(self._iterators[1])
-
-		if not self._test:
-			self._motorY = Motor(self.PV_y, name = "yMotor")
-			self._motorZ = Motor(self.PV_z, name = "zMotor")
-			self._motors = [Motor(self.PV_x, name = "xMotor"),
-			                self._get_vmotor_pair(self._motorY, self._motorZ)]
-		else:
-			self._motorY = _test_motor_factory("yMotor", speed=1)
-			self._motorZ = _test_motor_factory("zMotor", speed=1)
-			self._motors = [_test_motor_factory("xMotor", speed=1),
-			                self._get_vmotor_pair(self._motorY, self._motorZ)]
-		self._hooks = self._get_Hook()
-		self._Scan = IterScan(self._motors[0], self._motors[1],
-		                      self._iterators[0], self._iterators[1], 
-		                      self._hooks)
-		self._allMotors = [self._motors[0], self._motorY, self._motorZ]
-
+		self._imprintHooks = self._initHooks()
+		self._scan = IterScan(self._imprintHooks, 
+							  *(self._motors, self._postionIterators))
 
 	def _parseConfig(self):
 		"""Parses the config file and sets all the values for imprint"""
@@ -76,11 +54,11 @@ class Imprint(object):
 		self._deltas = self._parseFloatParam("Motors", "deltas")
 		self._numSteps = self._parseFloatParam("Motors", "numSteps")
 		self._useAttenuator = self._parseBoolParam("GasAttenuator",
-		                                           "useAttenuator")
+												   "useAttenuator")
 		self._attenutatorValues = self._parseFloatParam("GasAttenutator",
-		                                                "attenuatorValues")
+														"attenuatorValues")
 		self._loopOnMotorsAtten = self._parseIntParam("GasAttenuator", 
-		                                              "loopOnMotors")
+													  "loopOnMotors")
 		self._burstMode = self._parseBoolParam("Linac", "burstMode")
 		self._numShots = self._parseFloatParam("Linac", "numShots")
 		self._loopOnMotorsNumShots = self._parseIntParam("Linac", "loopOnMotors")
@@ -94,13 +72,13 @@ class Imprint(object):
 		"""Parses boolean cfg entries to make sure they are True or False."""
 		boolStr = self._parser.get(section, subSection)
 		if boolStr.lower() == "true" or boolStr.lower() == "t":
-		    return True
+			return True
 		elif bookStr.lower() == "false" or bookStr.lower() == "f":
-		    return False
+			return False
 		else:
-			raise ValueError("Entry for section '{0}', subsection '{1}' is not 
+			raise ValueError("Entry for section '{0}', subsection '{1}' is not \
 valid. Must be True/False or T/F (not case sensitive).").format(section,
-                                                                subsection)
+																subsection)
 		
 	def _parseMotorParams(self, section, subsection):
 		"""Parses the PV config entries into a list of strs and tuples."""
@@ -109,26 +87,26 @@ valid. Must be True/False or T/F (not case sensitive).").format(section,
 		motors = []
 		for motorPV in motorPVs:
 			if type(motorPV) is str:
-			    motors.append(Motor(motorPV, name = Pv.get(motorPV+".DESC")))
+				motors.append(Motor(motorPV, name = Pv.get(motorPV+".DESC")))
 			else:
-			    motors.append(VirtualMotor(motorPV))
+				motors.append(VirtualMotor(motorPV))
 		return motors
 
 	def _parseFloatParam(self, section, subsection):
 		"""Parses list/tuples of floats, subsituting string values for nan."""
 		floatStr = self._parser.get(section, subsection)
-	    floatEval = literal_eval(floatStr)
-	    floatList = []
-	    for val in floatList:
-		    try:
-			    floatList.append(float(val))
+		floatEval = literal_eval(floatStr)
+		floatList = []
+		for val in floatList:
+			try:
+				floatList.append(float(val))
 			except ValueError:
 				floatList.append(float('nan'))
-		    except TypeError:
-			    floatTuple = []
-			    for tupVal in floatTuple:
-				    try:
-					    floatTuple.append(float(tupVal))
+			except TypeError:
+				floatTuple = []
+				for tupVal in floatTuple:
+					try:
+						floatTuple.append(float(tupVal))
 					except ValueError:
 						floatTuple.append(float('nan'))
 				floatList.append(floatTuple)
@@ -149,7 +127,7 @@ valid. Must be True/False or T/F (not case sensitive).").format(section,
 		"""
 		numPositions, numMotors, numDeltas = [], [], []
 		for motor, pos, delta in zip(self._motors, self._initialPositions, 
-		                             self.deltas):
+									 self.deltas):
 			try:
 				numMotors.append(motor.numMotors)
 			except AttributeError:
@@ -164,18 +142,18 @@ valid. Must be True/False or T/F (not case sensitive).").format(section,
 				numDeltas.append(len([delta]))
 		if numMotors != numPositions:
 			raise SizeMismatchError("Motors", "initialPositions", numMotors,
-			                        initialPositions)
+									initialPositions)
 		elif numMotors != numDeltas:
 			raise SizeMismatchError("Motors", "Deltas", numMotors, numDeltas)			
 		elif len(numMotors) != len(self._numSteps):
 			raise SizeMismatchError("Motors", "numSteps", numMotors, 
-			                        self._numSteps)
+									self._numSteps)
 		if self._useAttenuator:
 			self._checkNumSteps(self._loopOnMotorsAtten, self._attenuatorValues,
-			                    "Attenutation")
+								"Attenutation")
 		if self._burstMode:
 			self._checkNumSteps(self._loopOnMotorsNumShots, self._numShots,
-			                    "numShots")
+								"numShots")
 		# Passed Check
 
 	def _checkNumSteps(self, loopOnMotors, vals, name):
@@ -184,123 +162,54 @@ valid. Must be True/False or T/F (not case sensitive).").format(section,
 		and numSteps.
 		"""
 		loopOnMotorBinary = [1 if x in loopOnMotors else 0 
-		                     for x in xrange(len(self._numSteps))]
+							 for x in xrange(len(self._numSteps))]
 		expectedNumVals = sum(loopVal*steps for loopVal, steps in zip(
 			loopOnMotorBinary, self._numSteps))
 		if expectedNumVals != len(vals):
 			raise SizeMismatchError("Expected Number of {0} Values".format(name),
-			                        "Number of inputted {0} Values".format(name),
-			                        expectedNumVals, len(self._numShots))
+									"Number of inputted {0} Values".format(name),
+									expectedNumVals, len(self._numShots))
 		
 	def _initIterators(self):
-		"""Creates a list of iterators that correspond to the inputted motors."""
+		"""
+		Creates a list of iterators that correspond to the inputted motors. It
+		can handle motor groups by returning izip-ped iterators for each motor.
+		"""
 		initPos = self._initialPositons
 		deltas = self._deltas
 		steps = self._numSteps
 		iterators = []
-                
+				
 		for pos, delta, step in zip(initPos, deltas, steps):
-                        try:
-				iterator = (self._addTup(pos, self._multTup(i,delta)) 
-				            for i in xrange(step))
-                                iterators.append*
-			except:
-                                pass
-                        
-	def _addTup(self, tupleA, tupleB):
-                """
-                Method that does elemental addition of tuples and returns a the sum as
-                a tuple of the same length.
-                """
-                tupA = np.array(self._checkTup(tupleA))
-                tupB = np.array(self._checkTup(tupleB))
-		return tuple(tupA + tupB)
+			if type(pos) is not tuple or type(delta) is not tuple:
+			    iterators.append((pos + delta*i for i in xrange(step)))
+			else:
+				iterator = []
+				for inPos, inDelta in zip(pos, delta):
+					iterator.append(inPos + inDelta*i for i in xrange(step))
+				iterators.append(izip(*iterator))
+		return iterators
 
-	def _multTup(self, tupleA, tupleB):
-		"""
-		Method that does elemental multiplication of tuples and returns the
-		product as a tupe of the same length.
-		"""
-                tupA = np.array(self._checkTup(tupleA))
-                tupB = np.array(self._checkTup(tupleB))
-		return tuple(tupA * tupB)
-
-        def _checkTup(self, tup):
-                """
-                Method that checks if the input is a tuple. If it isn't it returns the
-                value cast as a tuple.
-                """
-                if type(tup) is tuple:
-                        return tup
-                else:
-                        try:
-                                return tuple(tup)
-                        except TypeError:
-                                return tuple([tup])
-
-	def _get_Hook(self):
-		"""Returns a hook object that defines motor hooks"""
-		class imprint_Hook(object):
-			def __init__(self, attenuator, PV_attenuator, a_iterator, burst, 
-			             test, do_print, motors):
-				self._linac = Linac()
-				self._attenuator = attenuator
-				self._PV_attenuator = PV_attenuator
-				self._a_iterator = a_iterator
-				self._burst = burst
-				self._test = test
-				self._do_print = do_print
-				self._motors = motors
-				self.n_hook_calls = [0, 0, 0, 0]
-				self.pos_vectors = []
-			def pre_scan(self, scan):
-			    self.n_hook_calls[0] += 1
-			def post_scan(self, scan):
-			    self.n_hook_calls[1] += 1
-			def pre_step(self, scan):
-			    self.n_hook_calls[2] += 1
-			def post_step(self, scan):
-				self.n_hook_calls[3] += 1
-				self.pos_vectors.append(scan.get_positions())
-				if not self._test:
-					if self._attenuator:
-						try:
-							val = self._a_iterator.next()
-							Pv.put(PV_attenuator, val)
-							Pv.wait_for_value(PV_attenuator+".RBV", val)
-						except: pass
-					if self._burst:
-						self._linac.start_burst()
-						self._linac.
-				if self._do_print:
-					for motor in self._motors:
-						print motor.name, motor.wm()
-
-		return imprint_Hook(self._attenuator, self.PV_attenuator, 
-		                    self._a_iterator, self._burst, self._test, 
-		                    self._do_print, self._motors)
+	def _initHooks(self):
+		"""Returns a hook object that defines motor hooks."""
+		return imprintHooks(numshots         = self._numShots,
+		                    useAttenutator   = self._useAttenuator,
+		                    attenuatorValues = self._attenuatorValues,
+		                    burstMode        = self._burstMode,
+		                    useMotors        = self._useMotors,
+		                    verbose          = self._verbose,
+		                    motors           = self._motors)
 	
-	def test_imprint(self):
+	def test(self):
 		"""
-		Runs the test if test is set to true. If false, it reinitializes motors
-		as virtual motors and runs the test, reinitializing it back again when
-		it completes
+		Runs the scan in test mode. Will output the motor positions with each
+		iteration.
 		"""
-		self._Scan.do_print = self._do_print
-		if self._test:
-		    self._Scan.scan_mesh()
-		else:
-			self._test = True
-			self._init_params()
-			self._Scan.scan_mesh()
-			self._test = False
-			self._init_params()
+		self._scan.test_mesh(do_print = self._verbose)
 
 	def run(self):
 		"""Runs the scan"""
-		if not self._test:
-			self._Scan.scan_mesh()
-		else: print "Currently in test mode"
+		self._scan.scan_mesh(do_print = self._verbose)
 
 	@property
 	def path(self): return self._path
@@ -313,68 +222,46 @@ valid. Must be True/False or T/F (not case sensitive).").format(section,
 		else: print "Invalid path"
 
 	@property
-	def test(self):
-		return self._test
+	def useMotor(self):
+		return self._useMotor
 
-	@test.setter
-	def test(self, val):
+	@useMotor.setter
+	def useMotor(self, val):
 		if type(val) is bool:
-			self._test = val
+			self._useMotor = val
 			self._init_params()
 		else: print "Please enter True or False"
 
 	@property
-	def burst(self):
-		return self._burst
+	def burstMode(self):
+		return self._burstMode
 
-	@burst.setter
-	def burst(self, val):
+	@burstMode.setter
+	def burstMode(self, val):
 		if type(val) is bool:
-			self._burst = val
+			self._burstMode = val
 			self._init_params()
 		else: print "Please enter True or False"
 
 	@property
-	def status(self):
-	    return self._get_status()
+	def useAttenuator(self):
+		return self._useAttenuator
 
-	@status.setter
-	def status(self, val):
-		print "Error: Cannot set status"
-
-	@property
-	def attenuator(self):
-		return self._attenuator
-
-	@attenuator.setter
-	def attenuator(self, val):
+	@useAttenuator.setter
+	def useAttenuator(self, val):
 		if type(val) is bool:
-			self._attenuator = val
+			self._useAttenuator = val
 		else: print "Please enter True or False"
 
 	@property
-	def do_print(self):
-		return self._do_print
+	def verbose(self):
+		return self._verbose
 
-	@do_print.setter
-	def do_print(self, val):
+	@verbose.setter
+	def verbose(self, val):
 		if type(val) is bool:
-			self._do_print = val
+			self._verbose = val
 		else: print "Please enter True or False"
-
-	def _get_status(self):
-		"""Print imprint paramters"""
-		str = "Initial X position: {0}  Delta X: {1}".format(self.xfrom, self.deltaX)
-		str += "\nInitial Y position: {0}  Delta Y: {1}".format(self.yfrom, self.deltaY)
-		str += "\nInitial Y position: {0}  Delta Z: {1}".format(self.zfrom, self.deltaZ)
-		str += "\nAll attenuator Values:\n"
-		str += self.attenuatorString
-		str += "\nRange X: {0}  Range Y: {0}.".format(self.xRange, self.yRange)
-		str += "\nX Motor PV:    {0}".format(self.PV_x)
-		str += "\nY Motor PV:    {0}".format(self.PV_y)
-		str += "\nZ Motor PV:    {0}".format(self.PV_z)
-		str += "\nAttenuator PV: {0}".format(self.PV_attenuator)
-		return str
 
 class VirtualMotor(object):
 	"""Virtual motor class until the real one works."""
@@ -395,18 +282,81 @@ class VirtualMotor(object):
 			for motor, val in zip(self._motors, vals):
 				motor.mv(val)
 		else:
-			raise ValueError("Motor and position mismatch: {0} motors with {1}
+			raise ValueError("Motor and position mismatch: {0} motors with {1} \
 inputted motions.".format(len(self._motors), len(vals)))
 	def wm(self):
 		return [motor.wm() for motor in self._motors]
 	def wait(self):
 		for motor in self._motors:
 			motor.wait()
+
+class imprintHooks(object):
+	"""Hook class used by the imprint class."""
+	def __init__(self, **kwargs):
+		self._numShots = kwargs.get("numShots", None)
+		self._useAttenuator = kwargs.get("useAttenuator", False)
+		self._attenuatorValues = kwargs.get("attenuatorValues", None)
+		self._burstMode = kwargs.get("burstMode", False)
+		self._useMotors = kwargs.get("useMotors", True)
+		self._verbose = kwargs.get("verbose", False)
+		self._motors = kwargs.get("motors", None)
+		self._linac = Linac()
+		self._attenStatus = ""
+		self._linacStatus = ""
+		self.nHookCalls = [0, 0, 0, 0]
+		self.posVectors = []
+	def pre_scan(self, scan):
+		self.nHookCalls[0] += 1
+	def post_scan(self, scan):
+		self.nHookCalls[1] += 1
+	def pre_step(self, scan):
+		self.nHookCalls[2] += 1
+	def post_step(self, scan):
+		self.nHookCalls[3] += 1
+		self.pos_vectors.append(scan.get_positions())
+		if self._useMotors:
+			if self._useAttenuator:
+				try:
+					attenVal = self._attenutatorValues.next()
+					if not isnan(attenVal):
+						Pv.put(pvAttenuator, attenVal)
+						Pv.wait_for_value(pvAtenuator+".RBV", attenVal)
+						self._attenStatus = "Reached {0}.".format(
+							Pv.get(pvAtenuator+".RBV"))
+					else: self._attenStatus = "Got NaN."
+				except AttributeError:
+					self._attenStatus = "Invalid input used."
+				except:
+					self._attenStatus = "Unkown error occured."
+			else: self._attenStatus = "Not using gas attenuator."
+			if self._burstMode:
+				try:
+					numShotsVal = self._numShots.next()
+					if not isnan(numShotsVal):
+						self._linac.get_burst(n=numShotsVal)
+						self._linac.wait_for_shot(verbose=self._verbose)
+						self._linacStatus = "Requested {0} shots.".format(
+							numShotsVal)
+					else: self._linacStatus = "Got NaN."
+				except AttributeError:
+					self._linacStatus = "Invalid input used."
+				except:
+					self._linacStatus = "Unkown error occured."
+			else: self._linacStatus = "Not in burst mode."
+		if self._verbose:
+			print self._status()
+	def _status(self):
+		status = "Motor Positions:"
+		for motor in self._motors:					
+			status += "{0}: {1}".format(motor.name, motor.wm())
+		status += "Gas Attenutator: {0}".format(self._attenStatus)
+		status += "Linac: {0}".format(self._linacStatus)
+		return status				
 		
 # Exception Classes
 class Error(exception):
 	"""Base class for exceptions in this module."""
-    pass
+	pass
 
 class ConfigPathError(Error):
 	"""Exception raised if the inputted config path is invalid."""
@@ -426,22 +376,21 @@ class SizeMismatchError(Error):
 		self._lenA = lenA
 		self._lenB = lenB
 	def __str__(self):
-		return repr("Config values for '{0}' and '{1}' have incorrect corresponing
-sizes {2} and {3}.".format(self._nameA, self.nameB, self._lenA, self._lenB)
+		return repr("Config values for '{0}' and '{1}' have incorrect \
+corresponing sizes {2} and {3}.".format(self._nameA, self.nameB, 
+                                        self._lenA, self._lenB))
 	
 
-if __name__ == "__main__":
-	arguments = docopt(__doc__)
+# if __name__ == "__main__":
+# 	arguments = docopt(__doc__)
 	
-	if arguments["--burst"]:
-		print "Burst mode selected."
+# 	if arguments["--burst"]:
+# 		print "Burst mode selected."
 	
-	imprint = ImprintAngle(arguments["--path"], 
-	                       arguments["--burst"], 
-	                       arguments["--test"])
-	if not arguments["--test"]:
-		imprint.run()
-	else:
-		imprint.test_imprint()
-
-	
+# 	imprint = ImprintAngle(arguments["--path"], 
+# 	                       arguments["--burst"], 
+# 	                       arguments["--test"])
+# 	if not arguments["--test"]:
+# 		imprint.run()
+# 	else:
+# 		imprint.test_imprint()
